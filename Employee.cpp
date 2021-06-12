de2752441c2fc810ebb94ffe6293f4a06fa1c89c
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-06-12 19:46:06 +0430

    add constructor and cc and validate to Employee

--- Employee.cpp
+++ Employee.cpp
@@ -1,5 +1,26 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #include "Employee.h"
+
+Employee::Employee(const string &name, const string &id, const Address &address, int h, int ph, int wtd, int wd) :
+        Person(name, id, address) {
+    if (!validate(id)) {
+        cout << "Invalid input";
+        exit(1);
+    }
+    this->hourWork = h;
+    this->salaryPerHour = ph;
+    this->workToDo = wtd;
+    this->workDone = wd;
+}
+
+bool Employee::validate(string id) {
+    if (id[2] != '*')
+        return false;
+    return Person::validate(id);
+}
+
+Employee::Employee(const Employee &e) : Person(e) {
+    hourWork = e.hourWork;
+    salaryPerHour = e.salaryPerHour;
+    workToDo = e.workToDo;
+    workDone = e.workDone;
+}
--- Employee.h
+++ Employee.h
@@ -1,12 +1,23 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #ifndef COMPANY_PROJECT_EMPLOYEE_H
 #define COMPANY_PROJECT_EMPLOYEE_H
 
+#include <ostream>
+#include "istream"
+#include "Person.h"
 
-class Employee {
+class Employee : Person {
+protected:
+    int hourWork;
+    int salaryPerHour;
+    int workToDo;
+    int workDone;
+public:
+    explicit Employee(const string & = "", const string & = "90*11111", const Address & = Address(), int = 0, int = 0,
+                      int = 0, int = 0);
+
+    Employee(const Employee &);
+
+    bool validate(string string1) override;
 
 };
 
