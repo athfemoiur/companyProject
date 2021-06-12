25a8b7cfb06231421b9205c7cc0bbee58ad7c72c
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-06-12 19:52:25 +0430

    add Boss class

--- Boss.cpp
+++ Boss.cpp
@@ -1,5 +1,39 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #include "Boss.h"
+
+Boss::Boss(const string &name, const string &id, const Address &address, int h, int ph, int wtd, int wd, int n)
+        : Employee(name, id, address, h, ph, wtd, wd) {
+    this->numberOfEmployees = n;
+}
+
+Boss::Boss(const Boss &b) : Employee(b) {
+    numberOfEmployees = b.numberOfEmployees;
+}
+
+int Boss::getNumberOfEmployees() const {
+    return numberOfEmployees;
+}
+
+void Boss::setNumberOfEmployees(int numberOfEmployees) {
+    Boss::numberOfEmployees = numberOfEmployees;
+}
+
+ostream &operator<<(ostream &os, const Boss &boss) {
+    os << static_cast<const Employee &>(boss) << " numberOfEmployees: " << boss.numberOfEmployees;
+    return os;
+}
+
+istream &operator>>(istream &in, Boss &boss) {
+    in >> static_cast<Employee &>(boss) >> boss.numberOfEmployees;
+    return in;
+}
+
+Boss &Boss::operator=(const Boss &b) {
+    Employee::operator=(b);
+    numberOfEmployees = b.numberOfEmployees;
+    return *this;
+}
+
+double Boss::calculateSalary() {
+    return Employee::calculateSalary() * 1.15;
+}
+
--- Boss.h
+++ Boss.h
@@ -1,14 +1,30 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #ifndef COMPANY_PROJECT_BOSS_H
 #define COMPANY_PROJECT_BOSS_H
 
 
-class Boss {
+#include <ostream>
+#include "Employee.h"
 
+class Boss : public Employee {
+private:
+    int numberOfEmployees;
+public:
+    explicit Boss(const string & = "", const string & = "90*11111", const Address & = Address(), int = 0, int = 0,
+                  int = 0, int = 0, int= 0);
+
+    Boss(const Boss &);
+
+    int getNumberOfEmployees() const;
+
+    void setNumberOfEmployees(int numberOfEmployees);
+
+    friend ostream &operator<<(ostream &os, const Boss &boss);
+
+    friend istream &operator>>(istream &in, Boss &boss);
+
+    Boss &operator=(const Boss &);
+
+    double calculateSalary() override;
 };
-
 
 #endif //COMPANY_PROJECT_BOSS_H
