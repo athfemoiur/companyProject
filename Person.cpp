ead73ad8e93f29931de999da6aa7127de2f4c0b7
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-06-12 19:39:25 +0430

    add constructor and cc and validate to Person

--- Person.cpp
+++ Person.cpp
@@ -1,5 +1,41 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #include "Person.h"
+
+Person::Person(const string &name, const string &id, const Address &address) {
+    if (!validate(id)) {
+        cout << "Invalid id";
+        exit(1);
+    }
+    this->name = name;
+    this->id = id;
+    this->address = address;
+}
+
+Person::Person(const Person &p) {
+    name = p.name;
+    id = p.id;
+    address = p.address;
+}
+
+bool Person::validate(string id) {
+    // check the length
+    int len = id.length();
+    int temp = len - 7;
+    if (len < 8 || len > 10)
+        return false;
+    // check the first two chars
+    int firstTwo = stoi(id.substr(0, 2));
+    if (firstTwo < 84 || firstTwo > 99)
+        return false;
+    // check the next 1 or 2 or 3 chars if they are number or not
+    int i;
+    for (i = 2; i < temp + 2; ++i) {
+        if ((int) id[i] >= 48 && (int) id[i] <= 57)
+            return false;
+    }
+    // check the last 5 chars
+    for (int j = i; j <= len; ++j) {
+        if (id[j] >= '4' && id[j] <= '6')
+            return false;
+    }
+    return true;
+}
--- Person.h
+++ Person.h
@@ -1,13 +1,22 @@
-//
-// Created by athfemoiur on 6/12/21.
-//
-
 #ifndef COMPANY_PROJECT_PERSON_H
 #define COMPANY_PROJECT_PERSON_H
-
+#include "Address.h"
+#include "iostream"
+#include "ostream"
+#include "istream"
+using namespace std;
 
 class Person {
+protected:
+    string name;
+    string id;
+    Address address;
+public:
+    explicit Person(const string& = "", const string & = "90a11111", const Address& = Address());
 
+    Person(const Person&);
+
+    virtual bool validate(string);
 };
 
 
